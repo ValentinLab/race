cb354fbdcd28f849bc6029f63645f4e707989665
Valentin Perignon <valentin.perignon@orange.fr>
2019-12-14 01:28:49 +0100

    Ajout du seed

--- stats.sh
+++ stats.sh
@@ -1,31 +1,69 @@
 #!/bin/bash
 
-# Titre
-echo -e "Classement de ./$1\n"
+# Couleurs et format
+color_default="\033[39m"
+color_blue="\033[34m"
+color_red="\033[31m"
+format_default="\033[0m"
+format_bold="\033[1m"
 
-# Vérifier les paramètres
-if [ $# -ne 2 ]; then
-  echo "Erreur : Nombre de paramètres incorrecte."
+# Affichage de l'aide
+if [ $1 = "-h" ] || [ $1 = "--help" ]; then
+  echo -e "${color_blue}${format_bold}Statistiques pour Race${format_default}${color_default}"
+  echo "  Utilisation : ./stats.sh <JOUEUR> <ADVERSAIRE> <ITERATION>"
+  echo ""
+  echo "  Paramètres : <JOUEUR>      premier joueur à exécuter (exemple : race-hi)"
+  echo "               <ADVERSAIRE>  deuxième joueur à exécuter"
+  echo "               <ITERATION>   nombre d'exécutions"
   exit
 fi
+
+# Vérifier les paramètres
+if [ $# -ne 3 ]; then
+  echo -e "${color_red}Erreur${color_default} : Nombre de paramètres incorrecte."
+  exit
+fi
+player1=$1
+player2=$2
+iteration=$3
 
 # Créer un dossier tmp
 mkdir tmp
 
-# Éxecuter x fois ./race-server
+# Boucle d'exécution
+echo -e "${color_blue}${format_bold}Statistiques de ${player1} vs ${player2}${format_default}${color_default}\n"
 index=0
-while [ $index -lt $2 ]; do
-  echo "---[ Exécution $[$index+1]/$2"
-  ./race-server ./$1 ./race-hi >> tmp/all.txt 2>> tmp/error.txt
+victories=0
+defeats=""
+while [ $index -lt $3 ]; do
+  # Exécuter le fichier
+  echo "---[ Exécution $[$index+1]/$iteration"
+  ./race-server ./$player1 ./$player2 > tmp/all.txt 2>> tmp/error.txt
+
+  # Compter le nombre de fois où player1 arrive premier
+  current=`grep "(1) ./$player1" tmp/all.txt`
+  if [ -z "$current" ]; then
+    defeat=`grep "Seed" tmp/all.txt | cut -c7-`
+    defeats="${defeats} ${defeat}"
+  else
+    victories=$[$victories+1]
+  fi
+
   index=$[$index+1]
 done
 
-# Compter le nombre de fois où player arrive premier
-grep "(1) ./$1" tmp/all.txt > tmp/order.txt
-order=`wc -l tmp/order.txt`
+# Affichage su résultat
+echo -e "${color_blue}\nRésultat :${color_default}"
+echo    "./$player1 : $victories victoire(s)"
+echo    "./$player2 : $[$iteration - $victories] victoire(s)"
 
-# Affichage du résultat
-echo -e "\n${order:0:2} sur $index"
+# Affichage des seeds
+if [ -n defeats ]; then
+  echo -e "${color_blue}\nSeeds de défaite :${color_default}"
+  for seed in $defeats; do
+    echo " - $seed"
+  done
+fi
 
 # Supprimer le dossier tmp
 rm -rf tmp
