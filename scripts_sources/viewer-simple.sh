09f2aa9650cf93fb068986814f665e0e2a7476a1
Nathanaël <contact@nathanaelhoun.fr>
2019-12-17 14:20:18 +0100

    Script pour le viewer permet d'afficher les deux versions du viewer

--- scripts_sources/viewer-simple.sh
+++ scripts_sources/viewer-simple.sh
@@ -12,21 +12,26 @@ if [ "$1" == "-h" ] ||  [ "$1" == "--help" ]; then
         echo "Made by Nathanaël H. & Valentin P."
         echo ""
         echo "  Usage :"
-        echo "      viewer-simple [-s seed] <player1> [player2] [player3] [player4]"
+        echo "      viewer-simple [-v2] [-s seed] <player1> [player2] [player3] [player4]"
         echo "      viewer-simple (-h | --help)"
         echo ""
         echo "  Options : "
         echo "      <players> Path to an executable player (for example, './myplayer')"
+        echo "      [-v2] Use the race-viewer-v2 version"
         exit 0
     fi
 
-if [ "$#" -lt 1 ] || [ "$#" -gt 6 ] ; then
+if [ "$#" -lt 1 ] || [ "$#" -gt 7 ] ; then
     echo -e "${color_red}viewer-simple : Illegal number of parameters.${color_default} Please try 'viewer-simple --help'"
     exit 0
 fi
 
+if [ "$1" == "-v2" ]; then
+    args=$2" "$3" "$4" "$5" "$6" "$7
+    ./race-viewer-v2 & (sleep 1 && ./race-server $args) && fg
+    exit 0
+fi
+
 args=$1" "$2" "$3" "$4" "$5" "$6
-
-./race-viewer-v2 & (sleep 1 && ./race-server $args) && fg
-
+./race-viewer & (sleep 1 && ./race-server $args) && fg
 exit 0
